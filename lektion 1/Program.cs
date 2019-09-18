ad9b30dc7ad2765843a57f6225124a0991e9f733
Unknown <and8700and@gmail.com>
2019-09-18 10:02:29 +0200

    lektion 1

---
+++ lektion 1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lektion_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NearDuplicate("do not worry about your difficulties in mathematics", "i would not worry about your difficulties you can easily learn what is needed", 3);
+            
+        }
+
+
+        public static void NearDuplicate(string stringOne, string stringTwo, int shingle){
+
+            var stringOneWords = stringOne.Split();
+            var stringTwoWords = stringTwo.Split();
+
+            List<string> shingleSetOne = new List<string>();
+            List<string> shingleSetTwo = new List<string>();
+            
+            string temp = "";
+
+            for(int i = 0; i < stringOneWords.Length - shingle +1; i++){
+                for(int j = 0; j < shingle; j++) {
+                    if(i + j < stringOneWords.Length) {
+                        temp += stringOneWords[i + j] + " ";
+                    }
+                }
+                shingleSetOne.Add(temp);
+                temp = "";
+            }
+
+            for(int i = 0; i < stringTwoWords.Length - shingle +1; i++){
+                for(int j = 0; j < shingle; j++) {
+                    if(i + j < stringTwoWords.Length) {
+                        temp += stringTwoWords[i + j] + " ";
+                    }
+                }
+                shingleSetTwo.Add(temp);
+                temp = "";
+            }
+
+            Console.WriteLine(Jaccard(shingleSetOne.ToArray(), shingleSetTwo.ToArray()).ToString());
+        }
+
+        public static double Jaccard(string[] a, string[] b){
+
+            IEnumerable<string> union = a.Union(b);
+            IEnumerable<string> overlap = a.Intersect(b);
+            
+            var simi =  (double) overlap.ToList().Count / (double)union.ToList().Count;
+            return simi;
+        }
+    }
+}
+
