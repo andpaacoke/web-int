5e11ba0f0df9084d4fcfad38de5ddf7e379927bf
Andreas Stenshøj <and8700and@gmail.com>
2020-01-18 16:50:55 +0100

    Collaborative

---
+++ recommender/Collaborative/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Collaborative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
